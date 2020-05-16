bbfc80263b37bd1a4d7f77afc906d534cecc59d6
Nguyễn Đức Tân <kenjikatoji999@gmail.com>
2020-05-16 09:55:30 +0700

    add Realm

--- CoolJapanese/Model/Flashcard.swift
+++ CoolJapanese/Model/Flashcard.swift
@@ -7,10 +7,21 @@
 //
 
 import Foundation
-struct Flashcard {
-    let name: String
-    let check: Bool
-    let learned: Int
-    let total: Int
-    let color: String
+import RealmSwift
+class Flashcard : Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var learned: Int = 0
+    @objc dynamic var total: Int = 0
+    @objc dynamic var color: String = ""
+    @objc dynamic var words: [Words]?
+}
+class Word {
+    var remember : Bool = false
+    var content: String = ""
+    var note: String = ""
+}
+class Words : Object {
+    @objc dynamic var remember : Bool = false
+    @objc dynamic var content: String = ""
+    @objc dynamic var note: String = ""
 }
